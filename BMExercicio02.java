312db1edde1805566fc0bd1aa457611093dff07d
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-05-11 21:54:01 -0300

    enviando atividade prática 2 BM e corrigindo README

---
+++ BMExercicio02.java
@@ -0,0 +1,29 @@
+class BMExercicio02 {
+    public static void main(String[] args) {
+
+        int i;
+        i = 1;
+        System.out.println(i);
+
+        long j;
+        j = 2;
+        System.out.println(j);
+
+        float p;
+        p = 20.0f;
+        System.out.println(p);
+
+        double q;
+        q = 30.0;
+        System.out.println(q);
+
+        boolean b;
+        b = true;
+        System.out.println(b);
+
+        char c;
+        c = 'k';
+        System.out.println(c);
+
+    }
+}
--- README.md
+++ README.md
@@ -22,8 +22,10 @@ f) ( F ) boolean epar = (4%2=0)
 
 g) ( F ) char nome = “Maria”;
 
-h) ( V ) String b = “1”
+h) ( F ) String b = “1”
 
 i) ( F ) float x = 2.7;
 
 j) ( F ) int a = false;
+
+---
